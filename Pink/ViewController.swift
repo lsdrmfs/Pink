b9adab1f9e1a8525c11984780ef3c03e50a5bece
lsdrmfs <lsdrmfs@gmail.com>
2020-12-05 03:22:22 +0800

    Initial Commit

---
+++ Pink/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pink
+//
+//  Created by Johnny_fk on 2020/12/5.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
